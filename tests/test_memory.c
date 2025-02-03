a92cca0349f48340faaa9eec2ce85cdb02f8aeb6
Ethan Evans <ethanalexevans@gmail.com>
2025-02-02 18:47:40 -0800

    remove extra space

--- tests/test_memory.c
+++ tests/test_memory.c
@@ -10,7 +10,7 @@ bool test_clone(struct Allocator *scratch) {
     size_t s1_length = strlen(s1); // @LibC @TODO
     char *s2 = clone(s1, sizeof(char) * s1_length, scratch);
 
-    bool strings_are_equal   = strncmp(s1, s2, s1_length) == 0;  // @LibC @TODO
+    bool strings_are_equal   = strncmp(s1, s2, s1_length) == 0; // @LibC @TODO
     bool pointers_are_unique = ((intptr_t) s1) != ((intptr_t) s2);
 
     return strings_are_equal && pointers_are_unique;
